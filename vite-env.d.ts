dd4a4202586cd195a23d4effe41bba31a8ed1c1f
interastral <interastral.vision@gmail.com>
2026-03-27 02:39:12 +0000

    توضیح تغییر

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE: string
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
-  
+  readonly VITE_API_BASE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
